5906547fb7f7b80a084bda97c22a7f39e43497d8
rakhya <pravarakhya.chilukuri@gmail.com>
2018-02-19 16:11:07 -0500

    data preprocessing completed

--- Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -10,14 +10,32 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+#Data Preprocessing
+#Missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values="NaN", strategy="median", axis=0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+#Categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+#for multiple values in categorical data
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+#for binary values in categorical data
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+#sc_y = StandardScaler()
+#y_train = sc_y.fit_transform(y_train)
